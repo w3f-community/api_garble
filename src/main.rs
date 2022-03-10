4db715234df6cb8de84c2197c52dcfb632c12d5c
Nathan Prat <nathan.prat@gmail.com>
2022-03-10 18:08:32 +0100

    make the IPFS address configurable by flag

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+use clap::Parser;
 use tonic::transport::Server;
 
 mod garble_routes;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Where to reach the IPFS node
+    #[clap(long, default_value = "/ip4/127.0.0.1/tcp/5001")]
+    ipfs_server_multiaddr: String,
+}
 
 // TODO DRY server creation with the tests
 // cf https://github.com/hyperium/tonic/blob/4b0ece6d2854af088fbc1bdb55c2cdd19ec9bb92/tonic-web/tests/integration/tests/grpc.rs#L113
@@ -8,15 +17,16 @@ mod garble_routes;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
 
+    let args = Args::parse();
+
     // TODO configurable port
     let addr = "0.0.0.0:3000".parse().unwrap();
-    let ipfs_server_port = 5001;
-    let ipfs_server_multiaddr = format!("/ip4/127.0.0.1/tcp/{}", ipfs_server_port);
 
     let circuits_api = garble_routes::GarbleApiServerImpl {
-        ipfs_server_multiaddr: ipfs_server_multiaddr,
+        ipfs_server_multiaddr: args.ipfs_server_multiaddr,
     };
-    let circuits_api = garble_routes::interstellarpbapigarble::garble_api_server::GarbleApiServer::new(
+    let circuits_api =
+        garble_routes::interstellarpbapigarble::garble_api_server::GarbleApiServer::new(
             circuits_api,
         );
     // let greeter = InterstellarCircuitsApiClient::new(greeter);
