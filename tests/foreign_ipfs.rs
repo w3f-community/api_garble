479b87754a7432b5a94aa6eee40635d0faa1bc49
Nathan Prat <nathan.prat@gmail.com>
2022-03-11 12:10:19 +0100

    add license

--- tests/foreign_ipfs.rs
+++ tests/foreign_ipfs.rs
@@ -1,3 +1,19 @@
+// api_garble
+// Copyright (C) 2O22  Nathan Prat
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 // cf https://github.com/rs-ipfs/rust-ipfs/blob/master/tests/common/interop.rs
 // and https://github.com/rs-ipfs/rust-ipfs/blob/master/.github/workflows/ci.yml
 
