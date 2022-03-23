b2c64d3cab15b20525a5147c6f5586f5cc41b3e5
Nathan Prat <nathan.prat@gmail.com>
2022-03-23 11:47:51 +0100

    lib_garble_wrapper: refactor build
    - build.rs: use static linking
    - deps: move submodule deps/lib_garble/ to lib_garble_wrapper/deps/lib_garble/
    - deps: update lib_garble
    - export linked libraries from CMake
    - build.rs: use linker args obtained from CMake
    - Dockerfile: first pass for a proper/clean Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,67 @@
+################################################################################
+
+# podman build -f Dockerfile -t api_garble:dev --volume ~/.cargo:/root/.cargo:rw --volume $(pwd)/target/release:/usr/src/app/target/release:rw .
+# NOTE: it CAN work with Docker but it less than ideal b/c it can not reuse the host's cache
+# NOTE: when dev/test: if you get "ninja: error: loading 'build.ninja': No such file or directory"
+# -> FIX: find target/release/ -type d -name "*-wrapper-*" -exec rm -rf {} \;
+# b/c docker build has no support for volume contrary to podman/buildah
+# docker run -it --name api_garble --rm -p 3000:3000 --env RUST_LOG="warn,info,debug" api_garble:dev /usr/local/bin/api_garble --ipfs-server-multiaddr /ip4/172.17.0.1/tcp/5001
+
+FROM rust:1.59 as builder
+
+WORKDIR /usr/src/app
+
+# prereq: install CMake
+ENV PATH=$PATH:/opt/cmake/bin/
+RUN wget https://github.com/Kitware/CMake/releases/download/v3.22.3/cmake-3.22.3-linux-x86_64.sh && \
+    chmod +x cmake-3.22.3-linux-x86_64.sh && \
+    mkdir /opt/cmake/ && \
+    ./cmake-3.22.3-linux-x86_64.sh --skip-license --prefix=/opt/cmake/ && \
+    rm cmake-*.sh && \
+    cmake -version
+
+# prereq: install Ninja (ninja-build)
+RUN wget https://github.com/ninja-build/ninja/releases/download/v1.10.2/ninja-linux.zip && \
+    unzip ninja-linux.zip -d /usr/local/bin/ && \
+    rm ninja-linux.zip && \
+    ninja --version
+
+# "error: 'rustfmt' is not installed for the toolchain '1.59.0-x86_64-unknown-linux-gnu'"
+RUN rustup component add rustfmt
+
+RUN apt-get update && apt-get install -y \
+    libboost-filesystem-dev \
+    && rm -rf /var/lib/apt/lists/*
+
+COPY . .
+RUN cargo install --path .
+
+# MUST also get all the shared libs; using the CMake generated list of libs
+# cf https://github.com/Interstellar-Network/cmake/blob/main/export_libs.cmake
+# It SHOULD be empty for "lib_garble" but we might as well handle it just in case.
+# NOTE: if it fails with cp: will not overwrite just-created '/usr/local/lib/liblibyosys.so' with '/usr/src/app/target/release/build/lib-circuits-wrapper-a097322ac7999802/out/build/_deps/yosys_fetch-build/liblibyosys.so'
+# It probably means you are caching the container target/ by using a volume and there are multiple build dir
+# CHECK: find target/release/build/ -type d -name "*lib-garble-wrapper*"
+# If yes: DELETE the dup
+RUN cat $(find target/release/ -type f -name cmake_generated_libs) | tr " " "\n" |  grep "/usr/src/app/target/release/.*.so" > list_local_shared_libs && \
+    xargs --arg-file=list_local_shared_libs cp --target-directory=/usr/local/lib/ && \
+    rm list_local_shared_libs \
+    || echo "no shared libs to copy" && touch /usr/local/lib/no_shared_lib_to_copy
+
+################################################################################
+
+FROM ubuntu:20.04
+
+EXPOSE 3000
+
+ENV APP_NAME api_garble
+
+ENV LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+
+# NOTE if "no shared libs to copy" above; we  MUST add a random file else COPY fails with:
+# "copier: stat: ["/usr/local/lib/*.so"]: no such file or directory"
+# cf https://stackoverflow.com/questions/31528384/conditional-copy-add-in-dockerfile
+COPY --from=builder /usr/local/lib/no_shared_lib_to_copy /usr/local/lib/*.so /usr/local/lib/
+COPY --from=builder /usr/local/cargo/bin/$APP_NAME /usr/local/bin/$APP_NAME
+
+CMD ["sh", "-c", "$APP_NAME"]
--- lib_garble_wrapper/build.rs
+++ lib_garble_wrapper/build.rs
@@ -15,80 +15,59 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 use cmake::Config;
-use glob::glob;
-use std::path::Path;
+use rust_cxx_cmake_bridge::read_cmake_generated;
+
+// NOTE: check git history for a "working" version using shared libs
+// It worked locally but was a pain to deploy/package cf "DBUILD_SHARED_LIBS" below
 
 fn main() {
-    // TODO can we make it work with SHARED libs?
-    let garble_lib = Config::new("../deps/lib_garble/")
+    // BEFORE CMake: that will (among other things) generate rust/cxx.h that
+    // is needed to compile src/rust_wrapper.cpp
+    // ALTERNATIVELY we could add a git submodule for https://github.com/dtolnay/cxx/tree/master/include
+    cxx_build::bridge("src/lib.rs").compile("lib-garble-wrapper");
+
+    let rust_wrapper = Config::new(".")
         // TODO use IPO/LTO, at least in Release
         .configure_arg("-GNinja")
-        // NOTE: can NOT really link using static libs
-        // b/c the target circuit_lib by itself does nothing, so pretty much everything end up optimized out by the linker
-        // and we end up with A LOT of "undefined reference"
-        .configure_arg("-DBUILD_SHARED_LIBS=ON")
-        // TODO CMAKE_EXPORT_COMPILE_COMMANDS? instead of glob?
-        // TODO target "all"?
-        .build_target("garble_helper")
+        // NOTE: SHOULD NOT use shared libs
+        // b/c it makes really messy to package/deploy/dockerize
+        // Also makes it hard to debug and run tests from just this package while in parent crate.
+        // (ie Undefined Reference)
+        .configure_arg("-DBUILD_SHARED_LIBS=OFF")
+        .build_target("rust_wrapper")
         // without this(default to true) cmake is run every time, and for some reason this thrashes the build dir
         // which causes to recompile from scratch every time(for eg a simple comment added in lib.rs)
-        .always_configure(false)
+        .always_configure(false) // TODO always_configure
         .build();
-    // CHECK
-    println!("### garble_lib.display: {}", garble_lib.display());
-    // println!("cargo:rustc-link-search=native={}", garble_lib.display()); // USELESS
-    // rust_deps.display() = .../target/debug/build/lib-server-2cb77547fe2ce2c4/out
-    // but .a static libs are below in out/build
-    // TODO can we instead pass eg CMAKE_BINARY_DIR and have all .a in the same dir(no subdir)?
+
+    // rust_wrapper.display() = /home/.../api_garble/target/debug/build/lib-garble-wrapper-XXX/out
+    // but the final lib we want(eg librust_wrapper.a) is below in build/src/
+    // TODO remove? this is done as part of the loop below
     println!(
         "cargo:rustc-link-search=native={}/build/src/",
-        garble_lib.display()
+        rust_wrapper.display()
     );
+    println!("cargo:rustc-link-lib=static=rust_wrapper");
 
-    // will link to ALL the libs built by CMake...
-    // This is not ideal but at least:
-    // - it works
-    // - it avoids hardcoding a whole list of dependencies of "garble_lib"
-    for entry in glob(&format!("/{}/**/*.a", garble_lib.display()).to_string())
-        .unwrap()
-        .chain(glob(&format!("/{}/**/*.so", garble_lib.display()).to_string()).unwrap())
-    {
-        match entry {
-            Ok(static_lib_path) => {
-                println!("{:?}", static_lib_path.display());
+    // target/debug/build/lib-garble-wrapper-XXX/out/build/src/cmake_generated_libs
+    let cmake_generated_libs_str = std::fs::read_to_string(
+        &format!(
+            "/{}/build/src/cmake_generated_libs",
+            rust_wrapper.display()
+        )
+        .to_string(),
+    )
+    .unwrap();
 
-                // /home/.../api_circuits/target/debug/build/lib-garbke-wrapper-49025516ce40925e/out/build/_deps/abseil-build/absl/types/libabsl_bad_optional_access.a
-                // we want: absl_bad_optional_access
-                let liblib_name = static_lib_path.file_stem().unwrap();
-                let liblib_name_str: String = liblib_name.to_str().unwrap().into();
-                let lib_name_str = liblib_name_str.strip_prefix("lib").unwrap();
-                println!("## lib_name: {:?}", lib_name_str);
-                let dir = static_lib_path.parent().unwrap();
-                println!("## dir: {:?}", dir);
+    read_cmake_generated(&cmake_generated_libs_str);
 
-                println!("cargo:rustc-link-search=native={}", dir.display());
-                println!("cargo:rustc-link-lib={}", lib_name_str);
-            }
-            Err(e) => println!("{:?}", e),
-        }
-    }
+    // TODO get the system libs using ldd?
+    // println!("cargo:rustc-link-lib=readline");
 
-    cxx_build::bridge("src/lib.rs")
-        .file("src/rust_wrapper.cpp")
-        // C++20 else:
-        // error: no type named 'string_view' in namespace 'std'
-        .flag_if_supported("-std=c++20")
-        .flag_if_supported("-std=c++2a")
-        // TODO use a dynamic path from CMake above
-        .include("src")
-        .include("../deps/lib_garble/src/")
-        .compile("lib-garble-wrapper");
-
-    // TODO? but careful, we MUST recompile if the .cpp, the .h or any included .h is modified
+    // But careful, we MUST recompile if the .cpp, the .h or any included .h is modified
     // and using rerun-if-changed=src/lib.rs make it NOT do that
-    println!("cargo:rerun-if-changed=src/lib.rs");
-    println!("cargo:rerun-if-changed=src/rust_wrapper.h");
-    println!("cargo:rerun-if-changed=src/rust_wrapper.cpp");
-    println!("cargo:rerun-if-changed=../deps/lib_garble/src/");
+    println!("cargo:rerun-if-changed=src/");
+    println!("cargo:rerun-if-changed=deps/lib_garble/src/");
     println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=CMakeLists.txt");
 }
